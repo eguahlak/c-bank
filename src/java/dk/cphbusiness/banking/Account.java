594f23f3275fecd1213730ed7b36cecc30689a72
Anders Kalhauge <anders@kalhauge.dk>
2015-03-22 15:42:11 +0100

    movements added

--- src/java/dk/cphbusiness/banking/Account.java
+++ src/java/dk/cphbusiness/banking/Account.java
@@ -1,10 +1,14 @@
 package dk.cphbusiness.banking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
   private Bank bank;
   private Customer customer;
   private String number;
-  private int balance = 0;
+//  private int balance = 0;
+  private List<Movement> movements = new ArrayList<>();
   
   public Account(Bank bank, String number) {
     this.bank = bank;
@@ -18,8 +22,11 @@ public class Account {
 
   public void transfer(String targetNumber, int amount) {
     Account targetAccount = bank.findAccount(targetNumber);
-    this.balance -= amount;
-    targetAccount.balance += amount;
+//    this.balance -= amount;
+//    targetAccount.balance += amount;
+    Movement movement = new Movement(this, targetAccount, amount);
+    this.movements.add(movement);
+    targetAccount.movements.add(movement);
     }
   
   public boolean isInternal() {
@@ -35,11 +42,30 @@ public class Account {
     } 
   
   public int getBalance() {
+//    return balance;
+    int balance = 0;
+    for (Movement movement : movements) {
+      if (movement.source == this) balance -= movement.amount;
+      else balance += movement.amount;
+      }
     return balance;
     }
   
   public Customer getCustomer() {
     return customer;
+    }
+  
+  private static class Movement {
+    private final Account source;
+    private final Account target;
+    private final int amount;
+    
+    Movement(Account source, Account target, int amount) {
+      this.source = source;
+      this.target = target;
+      this.amount = amount;
+      }
+
     }
   
   }
